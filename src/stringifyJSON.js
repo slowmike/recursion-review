d3eacf23ddcd9db26feeea0bde828ae53239cefa
slowmike <aznwizard123@gmail.com>
2018-06-11 11:59:47 -0700

    completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,4 +5,29 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  if (typeof(obj) === 'number' || obj === null || typeof(obj) === 'boolean') {
+    return ''+obj;
+  }
+  if (typeof(obj) === 'string') {
+    return '"' + obj + '"';
+  }
+  if (Array.isArray(obj)) {
+    var temp = []; 
+    for (var i = 0; i < obj.length; i++) {
+      temp.push(stringifyJSON(obj[i]));
+    }
+    return '[' + temp.join(',') + ']';
+  }
+  if (typeof(obj) === 'object') {
+    var temp = [];
+    for (key in obj) {
+      //if(obj[key] !== undefined && typeof(obj[key]) !== 'function') {
+      if (obj[key] === undefined || typeof(obj[key]) === 'function') {
+        continue;
+      }  
+      temp.push(stringifyJSON(key) + ':' + tringifyJSON(obj[key]));
+      //}
+    }
+    return '{' + temp.join(',') + '}';
+  }
 };
